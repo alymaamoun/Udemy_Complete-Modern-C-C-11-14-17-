7e0749e9812090d49e0322bb365c01165641ee3e
Aly Mammoun <53837104+alymaamoun@users.noreply.github.com>
2023-12-25 17:06:46 +0200

    Add files via upload

---
+++ Section 6/Integer.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+class Integer
+{
+
+	int* m_pInt;
+public:
+	//Default constructor
+	Integer();
+	//Parameterized constructor
+	Integer(int val);
+	//Copy constructor
+	Integer(const Integer& val);
+	//Move constructor
+	Integer(Integer&& val)noexcept;
+	//Destructor
+	~Integer();
+
+
+	//getter
+	int GetValue(void)const;
+	//setter
+	void SetValue(int val);
+
+
+	//Copy assignment operator
+	Integer& operator=(const Integer& val);
+	//move assignment operator
+	Integer& operator=(Integer&& val)noexcept;
+	//add operator
+	Integer operator+(const Integer& val)const;
+	//prefix increment operator
+	Integer& operator++();
+	//postfix increment 
+	Integer operator++(int);
+	//compare operator
+	bool operator==(const Integer& val)const;
+	//arguments operator
+	void operator ()();
+	friend std::ostream& operator << (std::ostream& out, const Integer& a);
+	friend std::istream& operator >> (std::istream& in, Integer& a);
+	
+};
+
+
